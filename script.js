0412ae146668c60e5cec3e17b2c93f54b0cb1576
Allison Ang <allisonang21@gmail.com>
2022-12-03 14:11:14 -0800

    start button fx

--- script.js
+++ script.js
@@ -1,7 +1,38 @@
-const startButton = document.getElementById("start-btn");
-startButton.addEventListener("click", startQuiz);
+// reach into the html and grabe the start button
+var startButton = document.querySelector("#start-btn");
 
-function startQuiz() {
-  console.log("STAAAART");
-  startButton.classList.add("hide");
-}
+// sanity check
+console.log(startButton);
+console.dir(startButton);
+
+// when the start button is clicked
+startButton.addEventListener("click", function() {
+    // starting the time
+
+
+
+
+
+    console.log("START"); // sanity check
+    // show the question container
+    // reach into the html and grabe the question container
+    var questionContainerEl = document.querySelector('#questions-container');
+    console.dir(questionContainerEl)
+    // change the display property
+    questionContainerEl.style.display = 'block';
+
+
+    // show the first question
+    var question1Container = document.querySelector('#question-group1');
+    question1Container.style.display = 'block'
+
+
+    // hide the start button
+    // use the var that holds start button to change its style and display prop to none
+
+
+    // listen for clicks
+    // evaluate answer
+    // show next question group
+});
+
