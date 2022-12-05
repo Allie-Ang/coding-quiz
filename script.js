85385d2980c1deca35976ba7463e4a14e1fd90ab
Allison Ang <allisonang21@gmail.com>
2022-12-04 18:22:01 -0800

    adding alerts

--- script.js
+++ script.js
@@ -1,24 +1,50 @@
 // reach into the html and grabe the start button
 var startButton = document.querySelector("#start-btn");
 
+// Hiding Q1
 function renderQ2() {
   var question1Container = document.querySelector("#question-group1");
   question1Container.style.display = "none";
-
+  // Showing Q2
   var question2Container = document.querySelector("#question-group2");
   question2Container.style.display = "block";
-
+  // Q2 buttons
   var q2Btn = document.querySelectorAll(".q2Btn");
   for (var i = 0; i < q2Btn.length; i++) {
     q2Btn[i].addEventListener("click", function () {
       if (this.value == "true") {
         //increase score
-        console.log("correct");
+        alert("CORRECT!");
       } else {
         //deduct time
+        alert("TIME DEDUCTED :(");
         console.log("incorrect");
       }
       renderQ3();
+    });
+  }
+}
+
+// Q3
+function renderQ3() {
+  var question2Container = document.querySelector("#question-group2");
+  question2Container.style.display = "none";
+
+  var question3Container = document.querySelector("#question-group3");
+  question3Container.style.display = "block";
+
+  var q3Btn = document.querySelectorAll(".q3Btn");
+  for (var i = 0; i < q3Btn.length; i++) {
+    q3Btn[i].addEventListener("click", function () {
+      if (this.value == "true") {
+        //increase score
+        alert("CORRECT!");
+        console.log("correct");
+      } else {
+        //deduct time
+        alert("TIME DEDUCTED :(");
+        console.log("incorrect");
+      }
     });
   }
 }
@@ -46,45 +72,18 @@ startButton.addEventListener("click", function () {
 
   var q1Btn = document.querySelectorAll(".q1Btn");
 
-  //   q1Btn.forEach(function (btn) {
-  //     console.log(btn);
-  //     btn.addEventListener('click', function(){
-  //         console.log(this)
-  //     })
-  //   });
   for (var i = 0; i < q1Btn.length; i++) {
     q1Btn[i].addEventListener("click", function (event) {
-      //console.log(this);
-      //console.log(event.target.value);
-      //console.log(event);
       if (this.value == "true") {
         //increase score
+        alert("CORRECT!");
         console.log("correct");
       } else {
         //deduct time
+        alert("TIME DEDUCTED :(");
         console.log("incorrect");
       }
       renderQ2();
     });
   }
-  // listen for clicks
-  // evaluate answer
-  // show next question group
-  // Keep score
 });
-
-// inside for loop, create event listern for button
-// query selector for all
-// create a new function.
-
-// when clicking answer, moves on to next question, with alerts. Add event listener for answer buttons
-// var question2Btn = document.querySelector("#answer-btn2");
-// question2Btn.addEventListener("click", function () {
-//   question1Container.style.display = "none";
-// Add alert for right or wrong answer
-// Keep Score
-// Time function
-// });
-
-// Conditional statements.
-// Do I add a sep id for each button to present an alert and add an event listeners? in order to keep score as well?
